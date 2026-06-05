0136479c663118ff38177789bbbf58d4a06ca17d
umem1125 <chaerulumam1125@gmail.com>
2026-06-06 02:14:02 +0700

    fix: successfully configure astro ssr build for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  output: 'server',
   integrations: [mdx(), sitemap()],
 
   fonts: [
